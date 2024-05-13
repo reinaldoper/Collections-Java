7d54da210800965f610e7c917da9acbaaea8cbc7
reinaldoper <reinaldoper83@gmail.com>
2024-05-13 17:02:52 -0400

    feat: adiciona classe stream()

---
+++ src/operaçõesList/ListaTarefaStream.java
@@ -0,0 +1,30 @@
+package operaçõesList;
+
+import java.util.LinkedHashSet;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class ListaTarefaStream {
+    private Set<Tarefa> tarefaSet;
+
+    public ListaTarefaStream() {
+        this.tarefaSet = new HashSet<>();
+    }
+
+    public void adicionarTarefa(String descricao) {
+        tarefaSet.add(new Tarefa(descricao));
+    }
+
+    public void removerTarefa(String descricao) {
+        tarefaSet = tarefaSet.stream()
+                .filter(tarefa -> !tarefa.getDescricao().equals(descricao))
+                .collect(Collectors.toSet());
+    }
+
+    public void ordenarTarefas() {
+        tarefaSet = tarefaSet.stream()
+                .sorted()
+                .collect(Collectors.toCollection(LinkedHashSet::new));
+    }
+}
